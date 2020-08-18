cfbdc0b9089b2fa0092c85b1c4b9e4a9124037da
adhikary97 <adhikary@purdue.edu>
2020-08-18 01:11:07 -0500

    Updates

---
+++ precache-manifest.dcfc8e2ce38bed0b8a2127751903ec0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ead986c678bc9479c1fa544cc047c6d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b0d001decf60dfcb81f",
+    "url": "/static/css/main.a9ed3bb8.chunk.css"
+  },
+  {
+    "revision": "3ef77a8d02d24a2af366",
+    "url": "/static/js/2.d9d4b349.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.d9d4b349.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b0d001decf60dfcb81f",
+    "url": "/static/js/main.5111a3a1.chunk.js"
+  },
+  {
+    "revision": "a608daf20efb96070d93",
+    "url": "/static/js/runtime-main.63b068ac.js"
+  }
+]);
